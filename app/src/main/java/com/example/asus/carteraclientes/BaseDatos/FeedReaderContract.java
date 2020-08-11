0a9db6cb8a7c672e3d2036c5f986979bcefac75e
Angela Sucso <64700259+AngelaSucso@users.noreply.github.com>
2020-08-11 11:13:42 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/asus/carteraclientes/BaseDatos/FeedReaderContract.java
@@ -0,0 +1,15 @@
+package com.example.asus.carteraclientes.BaseDatos;
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract {
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "CLIENTES";
+        public static final String COLUMN_NAME = "NOMBRE";
+        public static final String COLUMN_DIREC = "DIRECCION";
+        public static final String COLUMN_EMAIL = "EMAIL";
+        public static final String COLUMN_NUMBER = "TELEFONO";
+    }
+}
