bf144075770ccbc230547909f143143aaf886085
Angela Sucso <64700259+AngelaSucso@users.noreply.github.com>
2020-08-11 11:14:19 -0500

    Update DatosOpenHelper.java

--- app/src/main/java/com/example/asus/carteraclientes/BaseDatos/DatosOpenHelper.java
+++ app/src/main/java/com/example/asus/carteraclientes/BaseDatos/DatosOpenHelper.java
@@ -3,27 +3,30 @@ package com.example.asus.carteraclientes.BaseDatos;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.asus.carteraclientes.BaseDatos.FeedReaderContract.FeedEntry;
 
 public class DatosOpenHelper extends SQLiteOpenHelper {
 
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE IF NOT EXISTS " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry.COLUMN_NAME + " VARCHAR(250), " +
+                    FeedEntry.COLUMN_DIREC + " VARCHAR(250), " +
+                    FeedEntry.COLUMN_EMAIL + " VARCHAR(200), " +
+                    FeedEntry.COLUMN_NUMBER + " VARCHAR(20))";
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "DATOS.db";
+
     public DatosOpenHelper(Context context){
-        super(context, "DATOS",  null,  1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase){
-        StringBuilder sql = new StringBuilder();
-        sql.append("CREATE TABLE IF NOT EXISTS CLIENTE (");
-        sql.append("NOMBRE VARCHAR(250), ");
-        sql.append("DIRECCION VARCHAR(250), ");
-        sql.append("EMAIL VARCHAR(200), ");
-        sql.append("TELEFONO VARCHAR(20))");
-
-        sqLiteDatabase.execSQL(sql.toString());
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int il){
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
     }
+
 }
